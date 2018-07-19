c2f2d9ba4d744d336c63770d2831008717c4facf
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-07-19 12:54:44 -0500

    Export test cases

--- problems.py
+++ problems.py
@@ -10,9 +10,9 @@ github = github3.login(os.environ["GITHUB_USERNAME"], os.environ["GITHUB_PASSWOR
 
 def download_problem_specifications():
     repo = github.repository("exercism", "problem-specifications")
-    problems = pandas.DataFrame({
-        "exercise": [filename for filename, _ in repo.directory_contents("exercises")]
-    })
+    problems = pandas.DataFrame(
+        {"exercise": [filename for filename, _ in repo.directory_contents("exercises")]}
+    )
 
     def load_canonical_data(name):
         try:
@@ -30,15 +30,18 @@ def download_problem_specifications():
         try:
             cases = pandas.DataFrame(problem_spec.canonical_data["cases"])
         except Exception as e:
-            print(e)
             cases = pandas.DataFrame()
         else:
-            cases["exercise"] = problem_spec.exercise
+            if "cases" in cases.columns:
+                cases = pandas.DataFrame()
+            else:
+                cases["exercise"] = problem_spec.exercise
         return cases
 
-    # test_cases = pandas.concat([melt_test_cases(x) for x in problems.itertuples()],
-    #         ignore_index=True)
-    # test_cases.to_csv("test-cases.csv", index=False)
+    test_cases = pandas.concat(
+        [melt_test_cases(x) for x in problems.itertuples()], ignore_index=True
+    )
+    test_cases[["exercise", "description"]].to_csv("test-cases.csv", index=False)
 
     del problems["canonical_data"]
     problems.to_csv("problem-specifications.csv", index=False)
@@ -46,9 +49,14 @@ def download_problem_specifications():
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--specifications', action='store_true',
-            help='download problem specifications')
+    parser.add_argument(
+        "-s",
+        "--specifications",
+        action="store_true",
+        help="download problem specifications",
+    )
     args = parser.parse_args()
     if args.specifications:
         download_problem_specifications()
